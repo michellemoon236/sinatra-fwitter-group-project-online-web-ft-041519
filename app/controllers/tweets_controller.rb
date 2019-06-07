16b2872c694249842f8ec96a14556795deaa71c4
Michelle Moon <michellemoon236@gmail.com>
2019-06-07 00:59:00 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,46 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      @tweets = current_user.tweets
+      erb :'tweets/index'   
+    else
+      redirect '/login'
+    end
+      
+  end
+ 
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+  
+  post '/tweets' do
+    @tweet = Tweet.create(:content => params[:content])
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do  #load edit form
+    @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/edit'
+    end
+ 
+  patch '/tweets/:id' do #edit action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+
+  delete '/tweets/:id' do #delete action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect '/tweets'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
 
-
+  get '/signup' do
+    erb :'users/signup'
+  end
+  
+  post '/signup' do
+    if params[:username] == "" || params[:email] =="" || params[:password] ==""
+      redirect "/signup"
+    else 
+      User.create(username: params[:username], email: params[:email], password: params[:password])
+      redirect '/tweets'
+    end
+  end 
+  
+  get '/login' do 
+    if logged_in?
+      redirect "/tweets" 
+    else 
+      erb :'users/login'
+    end
+  end
+  
+  post '/login' do
+    #binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+  
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
